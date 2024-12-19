c06aecbeab99c32d47e4e9cd17b435e9819f1463
alowkii <paalok333@gmail.com>
2024-12-20 02:21:18 +0530

    Printed the information on the web page.

--- src/displayData.js
+++ src/displayData.js
@@ -1,7 +1,26 @@
 export { displayWeatherData, displayWeatherDataErr }
 
 function displayWeatherData(data) {
-    console.log(data);
+    const datetime = data.currentConditions.datetime;
+
+    for(let i=0; i<data.days.length; i++) {
+        const date = data.days[i].datetime;
+        const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        const day = new Date(date).getDay();
+        const dayOfWeek = daysOfWeek[day];
+
+        const temp = data.days[i].tempmax;
+        const desc = data.days[i].conditions;
+
+        const weatherData = document.createElement('div');
+        weatherData.innerHTML = `
+            <h3>${dayOfWeek}</h3>
+            <p>Temperature: ${temp}°F</p>
+            <p>Conditions: ${desc}</p>
+        `;
+
+        document.getElementById('weather-data').appendChild(weatherData);
+    }
 }
 
 function displayWeatherDataErr(error) {
